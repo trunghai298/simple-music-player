95761cda676c8a2a37f3c5bc81e12e52364d87f2
haintse06350 <haint180298@gmail.com>
2022-11-09 01:46:57 +0700

    improve

--- music.js
+++ music.js
@@ -1,5 +1,9 @@
 const btnHome = document.querySelector(".go-home");
+const btnCloseLyrics = document.querySelector(".close-lyrics");
+
 const btnPlayList = document.querySelector(".play-list");
+const btnLyrics = document.querySelector(".lyrics");
+
 const btnPlay = document.querySelector(".play-song");
 const btnBack = document.querySelector(".back");
 const btnForward = document.querySelector(".forward");
@@ -7,7 +11,10 @@ const btnForward = document.querySelector(".forward");
 const audio = document.querySelector("#audio");
 const progress = document.querySelector(".progress");
 const playListBox = document.querySelector(".playlist-box");
+const lyricsBox = document.querySelector(".lyrics-box");
 const songs = document.querySelector(".list-song");
+const lyrics = document.querySelector(".song-lyrics");
+
 const thumbnailSong = document.querySelector(".thumbnail-song img");
 const songTb = document.getElementById("song-thumbnail");
 const nameSong = document.querySelector(".info-song .song-name");
@@ -16,13 +23,70 @@ const timeSong = document.querySelector(".bar-song .duration-time");
 const musicContent = document.querySelector(".music-content");
 const progressBar = document.querySelector(".progress-bar");
 const currentTimeDisplay = document.querySelector(".current-time");
-console.log("nameSong", nameSong);
+
 const listMusic = [
   {
     song: "Can't Take My Eyes Off You",
     author: "Jaehyun",
     src: "./assets/1.mp3",
     thumbnail: "./assets/IMG_1476.JPG",
+    lyrics: `
+You're just too good to be true
+I can't take my eyes off you
+You'd be like heaven to touch
+I wanna hold you so much
+At long last love has arrived
+And I thank God I'm alive
+You're just too good to be true
+Can't take my eyes off you
+Pardon the way that I stare
+There's nothing else to compare
+The sight of you leaves me weak
+There are no words left to speak
+But if you feel like I feel
+Please let me know that it's real
+You're just too good to be true
+I can't take my eyes off you
+I love you, baby
+And if it's quite all right
+I need you, baby
+To warm the lonely nights
+I love you, baby
+Trust in me when I say
+Oh, pretty baby
+Don't bring me down I pray
+Oh, pretty baby
+Now that I've found you, stay
+And let me love you, baby
+Let me love you
+You're just too good to be true
+I can't take my eyes off you
+You'd be like heaven to touch
+I wanna hold you so much
+At long last love has arrived
+And I thank God I'm alive
+You're just too good to be true
+Can't take my eyes off you
+I love you, baby
+And if it's quite all right
+I need you, baby
+To warm the lonely nights
+I love you, baby
+Trust in me when I say
+Oh, pretty baby
+Don't bring me down I pray
+Oh, pretty baby
+Now that I've found you, stay
+And let me love you, baby
+Let me love you
+You're just too good to be true
+I can't take my eyes off you
+You'd be like heaven to touch
+I wanna hold you so much
+At long last love has arrived
+And I thank God I'm alive
+You're just too good to be true
+Can't take my eyes off you`,
   },
   {
     song: "I Like Me Better",
@@ -46,7 +110,82 @@ const listMusic = [
     song: "Try Again",
     author: "Jaehyun",
     src: "./assets/5.mp3",
-    thumbnail: "./assets/IMG_1480.JPG",
+    thumbnail: "./assets/jh.jpg",
+    lyrics: `Romanization
+    uriga nanun
+    gin shiganmankeum neureonan gidaeneun
+    eojjeomyeon dangyeonhaljido molla
+    sumaneun wechimdo
+    seoroye mamen dachi motan chae
+    geureoke heulleogagido haetjiman
+    So whenever you ask me again
+    How I feel
+    Please remember
+    My answer is you
+    
+    meon gireul dashi doraganda haedo
+    nan yeojeonhi gateun mamil tenikka
+    We’ll be alright
+    I want to try again
+    
+    jajeun datume
+    myeot beonigo dashi muneojeodo
+    jungyohan geon urirago malhaetteut
+    muuimihaji ana
+    han georeum deo naaganeun georago
+    jinannari malhaejugo inneun geol
+    
+    So whenever you ask me again
+    How I feel
+    Please remember
+    My answer is you
+    
+    meon gireul dashi doraganda haedo
+    nan yeojeonhi gateun mamil tenikka
+    We’ll be alright
+    I want to try again
+    
+    gateun banbogieodo
+    You should know that
+    I’m always on your side
+    Please remember
+    My answer is you
+    
+    meon gireul dashi doraganda haedo
+    nan yeojeonhi gateun mamil tenikka
+    We’ll be alright
+    I want to try again
+    
+    We’ll be alright
+    Please try again`,
+  },
+  {
+    song: "BROKEN",
+    author: "GC BEAT",
+    src: "./assets/Broken.mp3",
+    thumbnail:
+      "https://cdn5.beatstars.com/eyJidWNrZXQiOiJidHMtY29udGVudCIsImtleSI6InVzZXJzL3Byb2QvMzYxMjI3L2ltYWdlL3c5M0JnUUdiRVlCWC9icm9rZW4ucG5nIiwiZWRpdHMiOnsicmVzaXplIjp7ImZpdCI6ImZpbGwiLCJ3aWR0aCI6MTYwLCJoZWlnaHQiOjE2MH19fQ==?t=1647018800235",
+  },
+  {
+    song: "ROSE",
+    author: "GC BEAT",
+    src: "./assets/rose.mp3",
+    thumbnail:
+      "https://cdn5.beatstars.com/eyJidWNrZXQiOiJidHMtY29udGVudCIsImtleSI6InVzZXJzL3Byb2QvMzYxMjI3L2ltYWdlL2J3dXd5aFhNdTlidC9yb3NlLnBuZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJmaXQiOiJmaWxsIiwid2lkdGgiOjE2MCwiaGVpZ2h0IjoxNjB9fX0=?t=1646683186824",
+  },
+  {
+    song: "SAY WHAT I WANT",
+    author: "GC BEAT",
+    src: "./assets/swiw.mp3",
+    thumbnail:
+      "https://cdn5.beatstars.com/eyJidWNrZXQiOiJidHMtY29udGVudCIsImtleSI6InVzZXJzL3Byb2QvMzYxMjI3L2ltYWdlL09nUFJjenRwRk1HdC9zYXl3aGF0aXdhbnQucG5nIiwiZWRpdHMiOnsicmVzaXplIjp7ImZpdCI6ImZpbGwiLCJ3aWR0aCI6MTYwLCJoZWlnaHQiOjE2MH19fQ==?t=1642956514111",
+  },
+  {
+    song: "CUPID",
+    author: "GC BEAT",
+    src: "./assets/cupid.mp3",
+    thumbnail:
+      "https://cdn5.beatstars.com/eyJidWNrZXQiOiJidHMtY29udGVudCIsImtleSI6InVzZXJzL3Byb2QvMzYxMjI3L2ltYWdlLzRhdzlxNk9QQndaUC9jdXBpZC5wbmciLCJlZGl0cyI6eyJyZXNpemUiOnsiZml0IjoiZmlsbCIsIndpZHRoIjoxNjAsImhlaWdodCI6MTYwfX19?t=1641917567373",
   },
 ];
 
@@ -63,6 +202,14 @@ class UI {
   hidePlayListBox() {
     playListBox.classList.remove("active");
   }
+  // show lyrics
+  showLyricsBox() {
+    lyricsBox.classList.add("active");
+  }
+  // show lyrics
+  hideLyricsBox() {
+    lyricsBox.classList.remove("active");
+  }
   // get duration
   getDuration(music) {
     return new Promise(function (resolve) {
@@ -76,35 +223,43 @@ class UI {
   // set list song
   async setSongs() {
     songs.innerHTML = "";
-
     for (let i = 0; i < listMusic.length; i++) {
       const music = new Audio(`${listMusic[i].src}`);
       const time = await this.getDuration(music);
-      console.log(time);
       // const time = { time: await this.getDuration(music) };
       // listMusic[i] = { ...listMusic[i], ...time };
       songs.insertAdjacentHTML(
         "beforeend",
         `<div class="info-song">
-                    <div class="left">
-                        <span class="name-song">${listMusic[i].song}</span>
-                        <span class="author">${listMusic[i].author}</span>
-                    </div>
-                    <div class="right">
-                        <span class="minutes">${time}</span>
-                    </div>
-                </div>`
+          <img class="song-small-thumbnail" src=${listMusic[i].thumbnail} alt=""/>
+          <div class="left">
+            <span class="name-song">${listMusic[i].song}</span>
+            <span class="author">${listMusic[i].author}</span>
+          </div>
+          <div class="right">
+            <span class="minutes">${time}</span>
+          </div>
+        </div>`
       );
     }
   }
   // load detail song when page loaded
   loadSong(music) {
     audio.src = music.src;
+    lyrics.innerHTML = "";
+    console.log(music);
+    lyrics.insertAdjacentHTML(
+      "beforeend",
+      ` <div class="lyrics">
+          <span>${music.lyrics}</span>
+        </div>`
+    );
     this.getDuration(audio).then((time) => {
       nameSong.textContent = music.song;
       author.textContent = music.author;
       timeSong.textContent = time;
       songTb.src = music.thumbnail;
+
       // thumbnailSong.classList.add("rotate-ani");
       audio.play();
     });
@@ -178,6 +333,7 @@ class UI {
     this.playSong();
 
     this.hidePlayListBox();
+    this.hideLyricsBox();
   }
 }
 
@@ -190,16 +346,24 @@ function eventListeners() {
   // handle set list song
   ui.setSongs();
   // handle show playlist
-  // btnPlayList.addEventListener("click", function () {
-  //   ui.showPlayListBox();
-  // });
+  btnPlayList.addEventListener("click", function () {
+    ui.showPlayListBox();
+  });
+
+  btnLyrics.addEventListener("click", function () {
+    ui.showLyricsBox();
+  });
+
   // handle hide playlist
   btnHome.addEventListener("click", function () {
     ui.hidePlayListBox();
   });
+
+  btnCloseLyrics.addEventListener("click", function () {
+    ui.hideLyricsBox();
+  });
   // play/pause song
   btnPlay.addEventListener("click", function () {
-    console.log("click ");
     if (musicContent.classList.contains("playing")) {
       ui.pauseSong();
     } else {
